0e8259e3436dd26a016241ba75cd5d53e0c9de29
Ahmed_barakat27 <ab560968@gmail.com>
2022-10-14 00:38:27 +0200

    Testing

--- smart_contract/contracts/Voting.sol
+++ smart_contract/contracts/Voting.sol
@@ -5,12 +5,12 @@ pragma experimental ABIEncoderV2;
 
 
 
-contract Voting{
-   
-
+contract  Voting{
    string public name;
    string public describetion;
    uint256 public numberOfCandidate=0;
+     uint public electionId=0;
+   
 
    struct Candidate{
     uint256 id;
@@ -21,20 +21,20 @@ contract Voting{
   
 
    mapping(uint=> Candidate) public candidates;
-
+   mapping(uint => address) public Elecitions;
    mapping(address=>bool) public voters;
-    constructor(string []memory _nda,string [] memory _candidates) public{ 
 
-    require(_candidates.length>0,"There most find at least ");
-    name=_nda[0];
-    describetion=_nda[1];
-
-    for(uint i=0;i<_candidates.length;i++){
-         addCandidate(_candidates[i]);
+    function createElecition(string [] memory _nda,string [] memory _candidats) public{
+          require(_candidats.length > 0, "There should be atleast 1 candidate.");
+          name = _nda[0];
+         describetion = _nda[1];
+       for(uint i = 0; i < _candidats.length; i++) {
+      addCandidate(_candidats[i]);
     }
+        Elecitions[electionId]=address(this);
+        electionId++;
 
-   }
- 
+    }
 
    function addCandidate(string memory _name) private{
      candidates[numberOfCandidate]=Candidate(numberOfCandidate,_name,0);
@@ -55,6 +55,21 @@ contract Voting{
    }
 
 
+   function getNumberOfCandidate()public view returns(uint num){
+    num=numberOfCandidate;
+    return num;
+   }
+   function getAllVoters()public view returns(bool ISVoted){
+   // require(!voters[msg.sender],"Voter is aleardy voted");
+    return voters[msg.sender];
+   }
+
+   function getAllCandidate() public view returns(Candidate [] memory){
+    
+   }
+
+
 }
+
 
 
--- smart_contract/test/Voting.js
+++ smart_contract/test/Voting.js
@@ -1,24 +1,61 @@
 
-const {ethers} = require("hardhat");
-const {expect} = require("chai");
+const { ethers } = require("hardhat");
+const { expect } = require("chai");
 
-describe("Task Contract", function() {
-    let VotingContract
-    let voting 
+describe("Voting Contract", function () {
+  let VotingContract
+  let voting
+  beforeEach(async function () {
+    VotingContract = await ethers.getContractFactory("Voting");
+    [addr1, addr2] = await ethers.getSigners();
+    voting = await VotingContract.deploy();
+    const candidate = [
+      {
+        "id": 0,
+        "name": "Ahmed",
+        "voteCount": 0
+      },
+      {
+        "id": 1,
+        "name": "Barakat",
+        "voteCount": 0
+      }
+    ]
+    const Candidate = ["USA Candidate", "USA Voting"]
+    await expect(await voting.connect(addr1).createElecition([Candidate[0], Candidate[1]], [candidate[0], candidate[1]]))
+  });
+
+
+  it("create candidate", async () => {
+    const candidate = [
+      {
+        "id": 0,
+        "name": "Ahmed",
+        "voteCount": 0
+      },
+      {
+        "id": 1,
+        "name": "Barakat",
+        "voteCount": 0
+      }
+    ]
+    const Candidate = ["USA Candidate", "USA Voting"]
+    await expect(await voting.connect(addr1).createElecition([Candidate[0], Candidate[1]], [candidate[0], candidate[1]]))
+  })
+  it("Make A Vote", async ()=>{
+      await expect(await voting.connect(addr1).vote(1))
+      console.log( await voting.getNumberOfCandidate())
+      
+  })
+  it("Already Voted", async ()=>{
+
+   await expect(await voting.connect(addr1).vote(1))
+   await expect(await voting.connect(addr1).vote(1))
+   console.log( await voting.getAllVoters());
     
-  
+})
 
-  beforeEach(async function() {
 
-    VotingContract =await ethers.getContractFactory("Voting");
-    [addr1,addr2]=await ethers.getSigners();
-    voting= VotingContract.deploy();
-   
-  });
 
-  describe("", function() {
-   
-  });
 
-  
 });
